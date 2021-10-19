402347fe46a1ed25c4c4d8b9141176c1c4113702
Artem <hamzinartem359@gmail.com>
2021-10-19 22:48:13 +0300

    added deleting video url; added more attempts to get video url

--- index.js
+++ index.js
@@ -5,27 +5,47 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 
 const bot = new Telegraf(BOT_CONFIG.TOKEN);
 
+const maxAttempts = 3;
 
 bot.on('text', (ctx) => {
 
-    console.log(`-> from ${ctx.message.from.first_name} ${ctx.message.from.last_name}: ${ctx.message.text}`)
+    console.log(`-> from ${ctx.message.from.first_name || ''} ${ctx.message.from.last_name || ''}: ${ctx.message.text}`)
 
     if (ctx.message.text.includes('tiktok.com')){
         bot.telegram.sendChatAction(ctx.chat.id, 'upload_video');
-        TikTokScraper.getVideoMeta(ctx.message.text, BOT_CONFIG.HEADERS)
-            .then(data =>{
-                fetch(data.collector[0].videoUrl, BOT_CONFIG.HEADERS)
-                    .then(res => res.buffer())
-                    .then(buffer => {
-                        console.log(`<- to ${ctx.message.from.first_name} ${ctx.message.from.last_name}: TikTok Video Success`)
-                        ctx.replyWithVideo({source: buffer}, { caption: data.collector[0].text })
-                    })
 
-            })
-            .catch(error => {
-                console.log(`<- to ${ctx.message.from.first_name} ${ctx.message.from.last_name}: TikTok Video Failed - ${error}`)
-                ctx.reply(`ERROR TikTok: ${error}`)
-            })
+        //attempt loop
+        for(let attempt = 0; attempt < maxAttempts; attempt++){
+
+            //Load video data
+            TikTokScraper.getVideoMeta(ctx.message.text, BOT_CONFIG.HEADERS)
+                .then(data =>{
+                    fetch(data.collector[0].videoUrl, BOT_CONFIG.HEADERS)
+                        .then(res => res.buffer())
+                        .then(buffer => {
+                            //end attempt loop
+                            attempt = maxAttempts;
+
+                            console.log(`<- to ${ctx.message.from.first_name} ${ctx.message.from.last_name}: TikTok Video Success`)
+                            //send video to chat
+                            ctx.replyWithVideo({source: buffer}, { caption: data.collector[0].text })
+                                .then(()=>{
+                                    //delete video url
+                                    ctx.deleteMessage(ctx.message.id);
+                                })
+                        })
+
+                })
+                .catch(error => {
+                    //if last attempt send error
+                    if (attempt === maxAttempts - 1){
+                        console.log(`<- to ${ctx.message.from.first_name || ''} ${ctx.message.from.last_name || ''}: TikTok Video Failed - ${error}`)
+                        ctx.reply(`ERROR TikTok: ${error}`)
+                    }
+
+                })
+        }
+
     }
 })
 
