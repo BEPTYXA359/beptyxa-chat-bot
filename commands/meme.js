a040a518346794583ff6009a8cf225956d89575d
beptyxa <hamzinartem359@gmail.com>
2022-07-22 17:49:57 +0300

    added gif and video meme

--- commands/meme.js
+++ commands/meme.js
@@ -11,15 +11,15 @@ let memeAlreadyEnabled = [];
 bot.command('enable_mem', async (ctx) => {
     ctx.reply('Влючаю мемификатор)')
     let memChatId = JSON.parse(await redisClient.get("memChatId")) || [];
-    if (!memChatId.includes(ctx.chat.id)){
+    if (!memChatId.includes(ctx.chat.id)) {
         memChatId.push(ctx.chat.id);
     }
-    redisClient.set("memChatId",  JSON.stringify(memChatId));
+    redisClient.set("memChatId", JSON.stringify(memChatId));
     enableMemeJob(ctx.chat.id);
 })
 bot.command('disable_mem', async (ctx) => {
     let memChatId = JSON.parse(await redisClient.get("memChatId")) || [];
-    if (memChatId.includes(ctx.chat.id)){
+    if (memChatId.includes(ctx.chat.id)) {
         memChatId.splice(memChatId.indexOf(ctx.chat.id), 1);
     }
     await redisClient.set("memChatId", JSON.stringify(memChatId));
@@ -27,43 +27,47 @@ bot.command('disable_mem', async (ctx) => {
 })
 bot.on("text", async (ctx) => {
     //случайная картинка в стиле "мем"
-    if (ctx.message.text.toLowerCase() === "мем"){
-        let mem;
-        try {
-            const response = await needle('get', 'https://www.anekdot.ru/random/mem/');
-            const $ = cheerio.load(response.body);
-            mem = $('.topicbox img').attr('src');
-            const image = await fetch(mem);
-            const buffer = await image.buffer();
-            const emoji = Math.random() * 100 > 70 ? phrases.emoji[Math.floor(Math.random() * phrases.emoji.length)] : '';
-            await ctx.replyWithPhoto({source: buffer}, {caption: `${phrases.mem[Math.floor(Math.random() * phrases.mem.length)]} ${emoji}`});
-        } catch (error) {
-            await ctx.reply(`Что то не так с мемом: ${error}`);
-        }
+    if (ctx.message.text.toLowerCase() === "мем") {
+        await sendMeme(ctx.chat.id);
     }
 })
-const enableMemeJob = function (chatId){
+const enableMemeJob = function (chatId) {
     if (memeAlreadyEnabled.includes(chatId)) return;
     memeAlreadyEnabled.push(chatId);
 
-    const memeJob = schedule.scheduleJob({hour:Math.floor(Math.random() * 24), minute:Math.floor(Math.random() * 60), tz: "Europe/Moscow"}, async () => {
-        let mem;
-        try {
-            const response = await needle('get', 'https://www.anekdot.ru/random/mem/');
-            const $ = cheerio.load(response.body);
-            mem = $('.topicbox img').attr('src');
-            const image = await fetch(mem);
-            const buffer = await image.buffer();
-            const emoji = Math.random() * 100 > 70 ? phrases.emoji[Math.floor(Math.random() * phrases.emoji.length)] : '';
-            await bot.telegram.sendPhoto(chatId, {source: buffer}, {caption: `${phrases.randomTimeMem[Math.floor(Math.random() * phrases.randomTimeMem.length)]} ${emoji}`});
-        } catch (error) {
-            await bot.telegram.sendMessage(chatId,`Что то не так с мемом: ${error}`);
-        }
+    const memeJob = schedule.scheduleJob({
+        hour: Math.floor(Math.random() * 24),
+        minute: Math.floor(Math.random() * 60),
+        tz: "Europe/Moscow"
+    }, async () => {
+        await sendMeme(null, chatId);
 
         memeAlreadyEnabled.splice(memeAlreadyEnabled.indexOf(chatId), 1)
         memeJob.cancel();
         enableMemeJob(chatId);
     });
+}
+
+const sendMeme = async (chatId) => {
+    let mem;
+    try {
+        const response = await needle('get', 'https://www.anekdot.ru/random/mem/');
+        const $ = cheerio.load(response.body);
+        mem = $('.topicbox img, .topicbox video source').attr('src');
+        const image = await fetch(mem);
+        const buffer = await image.buffer();
+        const emoji = Math.random() * 100 > 60 ? phrases.emoji[Math.floor(Math.random() * phrases.emoji.length)] : '';
+
+        if (mem.endsWith(".gif")) {
+            await bot.telegram.sendAnimation(chatId, {source: buffer}, {caption: `${phrases.mem[Math.floor(Math.random() * phrases.mem.length)]} ${emoji}`});
+        } else if(mem.endsWith(".mp4")) {
+            await bot.telegram.sendVideo(chatId, {source: buffer}, {caption: `${phrases.mem[Math.floor(Math.random() * phrases.mem.length)]} ${emoji}`})
+        } else {
+            await bot.telegram.sendPhoto(chatId, {source: buffer}, {caption: `${phrases.mem[Math.floor(Math.random() * phrases.mem.length)]} ${emoji}`});
+        }
+    } catch (error) {
+        await bot.telegram.sendMessage(chatId, `Что то не так с мемом: ${error}`);
+    }
 }
 
 module.exports.enableMemeJob = enableMemeJob;
