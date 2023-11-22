d69a9a440e79992438d5002b68e6aa1e8a5a4b54
beptyxa <hamzinartem359@gmail.com>
2023-11-23 02:40:34 +0300

    add try catch for openai

--- commands/openai.js
+++ commands/openai.js
@@ -3,43 +3,51 @@ let store = require('store');
 let openai = null;
 
 async function connectOpenAI() {
-    const { OpenAI } = await import("openai")
+    const {OpenAI} = await import("openai")
     openai = new OpenAI();
 }
 
 bot.hears(/^чатгпт(.*)/i, async ctx => {
     console.log("OpenAI Ответь:" + ctx.message.text)
-    bot.telegram.sendChatAction(ctx.chat.id, 'typing');
-    let history = store.get(ctx.chat.id) || [];
-    history.push(
-        { role: 'user', content: ctx.message.text.toLowerCase().replace("чатгпт", "") }
-    )
-    const chatCompletion = await openai.chat.completions.create({
-        messages: history,
-        model: 'gpt-4-1106-preview',
-    });
-    history.push(
-        { role: 'assistant', content: chatCompletion.choices[0].message.content }
-    )
-    if (history.length > 10) {
-        history.shift();
+    try {
+        bot.telegram.sendChatAction(ctx.chat.id, 'typing');
+        let history = store.get(ctx.chat.id) || [];
+        history.push(
+            {role: 'user', content: ctx.message.text.toLowerCase().replace("чатгпт", "")}
+        )
+        const chatCompletion = await openai.chat.completions.create({
+            messages: history,
+            model: 'gpt-4-1106-preview',
+        });
+        history.push(
+            {role: 'assistant', content: chatCompletion.choices[0].message.content}
+        )
+        if (history.length > 10) {
+            history.shift();
+        }
+        store.set(ctx.chat.id, history);
+        await ctx.reply(chatCompletion.choices[0].message.content);
+    } catch (error) {
+        await bot.telegram.sendMessage(process.env.ADMIN_ID, `Что то не так с openai у ${ctx.chat.id}: ${error}`);
     }
-    store.set(ctx.chat.id, history);
-    await ctx.reply(chatCompletion.choices[0].message.content);
 })
 
 bot.hears(/^[\s\S]*/i, async ctx => {
-    console.log("Chatterbox:" + ctx.message.text)
-    if (Math.random() * 100 > 5) return;
-    bot.telegram.sendChatAction(ctx.chat.id, 'typing');
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [
-            { role: 'system', content: process.env.OPENAI_CHATTERBOX_SYSTEM_TEXT },
-            { role: 'user', content: ctx.message.text }
-        ],
-        model: 'gpt-3.5-turbo',
-    });
-    await ctx.reply(chatCompletion.choices[0].message.content);
+    try {
+        console.log("Chatterbox:" + ctx.message.text)
+        if (Math.random() * 100 > 5) return;
+        bot.telegram.sendChatAction(ctx.chat.id, 'typing');
+        const chatCompletion = await openai.chat.completions.create({
+            messages: [
+                {role: 'system', content: process.env.OPENAI_CHATTERBOX_SYSTEM_TEXT},
+                {role: 'user', content: ctx.message.text}
+            ],
+            model: 'gpt-3.5-turbo',
+        });
+        await ctx.reply(chatCompletion.choices[0].message.content);
+    } catch (error) {
+        await bot.telegram.sendMessage(process.env.ADMIN_ID, `Что то не так с chatterbox у ${ctx.chat.id}: ${error}`);
+    }
 })
 
 module.exports = {
