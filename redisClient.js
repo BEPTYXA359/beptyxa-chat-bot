8ac4e1109e519c9a07fd8cd94a0e87068a92bbe7
beptyxa <hamzinartem359@gmail.com>
2023-11-23 02:37:51 +0300

    add connectTimeout for redis

--- redisClient.js
+++ redisClient.js
@@ -1,7 +1,7 @@
 const Redis = require('ioredis');
 const redisURL = process.env.REDIS_URL;
 
-const redisClient = redisURL ? new Redis(redisURL) : new Redis();
+const redisClient = redisURL ? new Redis({host: redisURL, connectTimeout: 10000}) : new Redis({connectTimeout: 10000});
 redisClient.on("connect", async ()=>{
     console.log("redis connected to", process.env.REDIS_URL)
 })
